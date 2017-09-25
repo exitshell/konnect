1508d39f4d4bf4749bc5c6f0ac2a855ef389fa48
Sandeep Jadoonanan <jsanweb@gmail.com>
2017-09-24 22:48:18 -0400

    Remove log formatting.

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"github.com/tunedmystic/konnect/cmd"
 )
 
+func init() {
+	// https://goo.gl/nPMoCL
+	log.SetFlags(0)
+}
+
 func main() {
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
